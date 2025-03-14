529b11225a5399b881121961302c18dec63bd9cd
CHANDRAHAS K <chandrahask535@gmail.com>
2025-03-14 17:22:16 +0530

    commit day2

---
+++ ooppss.demo/src/ooppss/demo/Encap_main.java
@@ -0,0 +1,16 @@
+package ooppss.demo;
+
+public class Encap_main {
+
+	public static void main(String[] args) {
+		
+		// Object creation
+		Encapdemo obj = new Encapdemo(); // new is keywords which is used to create a object
+		obj.setSerialNum(101);
+		obj.setName("Bunny");
+		obj.setAge(15);
+		System.out.println(obj);
+		
+	}
+
+}
